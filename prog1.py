1157e4ca4b5bc0d320bfd7a5dc4dc753897ecc0d
vishnupriyatct <117311094+vishnupriyatct@users.noreply.github.com>
2022-11-03 07:22:35 +0530

    Add files via upload

---
+++ prog1.py
@@ -0,0 +1,13 @@
+'''a=10
+b=20
+c=a+b
+print("The result is:\n",c)
+#print(c)
+print("the sum of",a,"and",b,"is",c)
+input()'''
+a=input("enter a:")
+b=input("enter b:")
+print(a+b)
+a=int(input("Enter the value of a:"))
+b=int(input("Enter the value of b:"))
+print("result=",a+b)
